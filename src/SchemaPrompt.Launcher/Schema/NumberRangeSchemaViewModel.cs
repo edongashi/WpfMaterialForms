f1684ea0bf879b18f8214b95cebb600f174b5f49
EdonGashi <edongashi@hotmail.co.uk>
2016-06-07 03:20:47 +0200

    Slider control

---
+++ src/SchemaPrompt.Launcher/Schema/NumberRangeSchemaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+using SchemaPrompt.Launcher.Validation;
+using System.Windows;
+
+namespace SchemaPrompt.Launcher.Schema
+{
+    public class NumberRangeSchemaViewModel : BaseSchemaViewModel
+    {
+        public int Value { get; set; }
+    }
+}
