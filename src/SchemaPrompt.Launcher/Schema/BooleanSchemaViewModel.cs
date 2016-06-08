533b7527b05c9774d184b9a204fcc9aefa037ee5
EdonGashi <edongashi@hotmail.co.uk>
2016-06-08 02:11:10 +0200

    SwitchControl

---
+++ src/SchemaPrompt.Launcher/Schema/BooleanSchemaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+using SchemaPrompt.Launcher.Validation;
+using System.Windows;
+
+namespace SchemaPrompt.Launcher.Schema
+{
+    public class BooleanSchemaViewModel : BaseSchemaViewModel
+    {
+        public bool Value { get; set; }
+    }
+}
