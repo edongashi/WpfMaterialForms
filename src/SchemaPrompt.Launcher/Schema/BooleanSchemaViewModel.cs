6ae17f0092b3a92d119d7903703ac523cd14956e
EdonGashi <edongashi@hotmail.co.uk>
2016-06-10 23:34:09 +0200

    KeyValue and Button control

--- src/SchemaPrompt.Launcher/Schema/BooleanSchemaViewModel.cs
+++ src/SchemaPrompt.Launcher/Schema/BooleanSchemaViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Windows.Controls;
-using MaterialDesignThemes.Wpf;
-using SchemaPrompt.Launcher.Validation;
-using System.Windows;
-
-namespace SchemaPrompt.Launcher.Schema
+﻿namespace SchemaPrompt.Launcher.Schema
 {
     public class BooleanSchemaViewModel : BaseSchemaViewModel
     {
