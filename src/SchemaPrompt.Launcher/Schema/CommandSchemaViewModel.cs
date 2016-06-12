ac6f5d139550795e23cc891a67974b86d61c17e9
EdonGashi <edongashi@hotmail.co.uk>
2016-06-12 02:33:19 +0200

    Command Schema

---
+++ src/SchemaPrompt.Launcher/Schema/CommandSchemaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+using SchemaPrompt.Launcher.Validation;
+using System.Windows;
+
+namespace SchemaPrompt.Launcher.Schema
+{
+    public class CommandSchemaViewModel : BaseSchemaViewModel
+    {
+        public string CommandHint { get; set; } = "Action";
+    }
+}
