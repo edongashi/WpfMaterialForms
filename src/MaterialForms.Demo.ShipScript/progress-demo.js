b843c316f5557c06532393abcf1187b16f5a469b
EdonGashi <edongashi@hotmail.co.uk>
2016-07-04 02:58:16 +0200

    Format progress-demo.js

--- src/MaterialForms.Demo.ShipScript/progress-demo.js
+++ src/MaterialForms.Demo.ShipScript/progress-demo.js
@@ -3,29 +3,29 @@ const forms = require('MaterialForms');
 const timer = require('timer');
 
 const result = forms.task(function (progress) {
-	
-	for (var i = 0; i <= 100; i++) {
-	    if (progress.cancelled) {
+
+    for (var i = 0; i <= 100; i++) {
+        if (progress.cancelled) {
             // Check if cancellation has been requested
-			return false;
-		}
-		
-		if (i === 50) {
-			progress.message = 'Almost finished...';
-		}
-		
-	    // Report current progress (out of 100)
-		progress(i);
-		timer.block(50);
-	}
-	
+            return false;
+        }
+
+        if (i === 50) {
+            progress.message = 'Almost finished...';
+        }
+
+        // Report current progress (out of 100)
+        progress(i);
+        timer.block(50);
+    }
+
     // Returns this value to the caller of forms.task()
-	return true;
-	
-}, { message: 'Working', cancel: 'CANCEL' } );
+    return true;
+
+}, { message: 'Working', cancel: 'CANCEL' });
 
 if (result) {
-	forms.alert("Completed successfully!");
+    forms.alert("Completed successfully!");
 } else {
-	forms.alert("Canceled!");
+    forms.alert("Canceled!");
 }
