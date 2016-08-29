69f253022f905ebf1a0a6302ece36383ea98200f
EdonGashi <edongashi@hotmail.co.uk>
2016-08-29 20:50:03 +0200

    DialogFactory & WPF Demo

--- src/MaterialForms.WpfDemo/MainWindow.xaml.cs
+++ src/MaterialForms.WpfDemo/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 using MaterialDesignThemes.Wpf;
 using MaterialForms.Tasks;
@@ -11,244 +10,92 @@ namespace MaterialForms.WpfDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const double ModalWidth = 250d;
+        public const double LargeModalWidth = 350d;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+        private async void ShowAlert(object sender, RoutedEventArgs e)
         {
-            var nameSchema = new StringSchema();
-            var window = new MaterialWindow(new MaterialDialog
-            {
-                Title = "Demo material forms",
-                Form = new MaterialForm
-                {
-                    new CommandSchema
-                    {
-                        Name = "Login window",
-                        CommandHint = "SHOW",
-                        Callback = ShowLogin
-                    },
-                    new CommandSchema
-                    {
-                        Name = "Settings window",
-                        CommandHint = "SHOW",
-                        Callback = ShowDemo(SettingsDialog)
-                    },
-                    new CommandSchema
-                    {
-                        Name = "2x settings window (data binding)",
-                        CommandHint = "SHOW",
-                        Callback = ShowDemo(SettingsDialog, true)
-                    },
-                    new CommandSchema
-                    {
-                        Name = "Dialog inside window",
-                        CommandHint = "SHOW",
-                        Callback = async args =>
-                        {
-                            await ((WindowSession)args.Session).ShowDialog(new MaterialDialog
-                            {
-                                Message = "Discard draft?",
-                                PositiveAction = "DISCARD"
-                            }, 250d);
-                        }
-                    },
-                    new CommandSchema
-                    {
-                        Name = "E-mail window",
-                        CommandHint = "SHOW",
-                        Callback = ShowDemo(EmailDialog)
-                    },
-                    new CommandSchema
-                    {
-                        Name = "Modal 2x schema (data binding)",
-                        CommandHint = "SHOW",
-                        Callback = async args => await ((WindowSession)args.Session).ShowDialog(new MaterialDialog(nameSchema, nameSchema), 250d)
-                    }
-                }
-            });
-
-            await window.Show();
+            await WindowFactory.Alert("Hello World!").Show();
         }
 
-        public static async void ShowLogin(object nothing)
+        private async void ShowModalAlert(object sender, RoutedEventArgs e)
         {
-            var window = new MaterialWindow(new MaterialDialog
-            {
-                Title = "Please log in to continue",
-                Form = new MaterialForm
-                {
-                    new StringSchema
-                    {
-                        Name = "Username",
-                        Key = "user",
-                        IconKind = PackIconKind.Account,
-                        Validation = Validators.IsNotEmpty
-                    },
-                    new PasswordSchema
-                    {
-                        Name = "Password",
-                        Key = "pass",
-                        IconKind = PackIconKind.Key
-                    },
-                    new BooleanSchema
-                    {
-                        Name = "Remember me",
-                        IsCheckBox = true
-                    }
-                },
-                PositiveAction = "LOG IN",
-                OnPositiveAction = async session =>
-                {
-                    // Simulate asynchronous work
-                    await Task.Delay(2000);
-                    var userCorrect = (string)session["user"] == "test";
-                    var passwordCorrect = (string)session["pass"] == "123456";
-                    if (userCorrect && passwordCorrect)
-                    {
-                        session.Close(true);
-                    }
-                    else
-                    {
-                        if (!userCorrect)
-                        {
-                            session.Invalidate("user", "This username does not exist.");
-                        }
-                        else
-                        {
-                            session.Invalidate("pass", "Invalid password.");
-                        }
-                    }
-                },
-                ShowsProgressOnPositiveAction = true
-            });
+            await DialogFactory.Alert("Hello World!").Show("RootDialog", ModalWidth);
+        }
 
-            var result = await window.Show();
+        private async void ShowPrompt(object sender, RoutedEventArgs e)
+        {
+            bool? result = await WindowFactory.Prompt("Delete item?").Show();
             if (result == true)
             {
-                var formData = window.Dialog.Form.GetValuesList();
-                await WindowFactory.Alert($"Username: {formData[0]}\nPassword: {formData[1]}\nRemember me: {formData[2]}", "Positive").Show();
-            }
-            else
-            {
-                await WindowFactory.Alert("Dialog dismissed", "Negative").Show();
+                await WindowFactory.Alert("Item deleted.").Show();
             }
         }
 
-        public static Action<object> ShowDemo(Func<MaterialDialog> dialog, bool showTwice = false)
+        private async void ShowModalPrompt(object sender, RoutedEventArgs e)
         {
-            Action<object> callback = arg =>
+            bool? result = await DialogFactory.Prompt("Delete item?").Show("RootDialog", ModalWidth);
+            if (result == true)
             {
-                var window = new MaterialWindow
-                {
-                    Dialog = dialog(),
-                };
-
-                window.Show();
-                if (showTwice)
-                {
-                    window.Show();
-                }
-            };
-
-            return callback;
+                await DialogFactory.Alert("Item deleted.").Show("RootDialog", ModalWidth);
+            }
         }
 
-        public static MaterialDialog SettingsDialog()
+        private async void ShowInput(object sender, RoutedEventArgs e)
         {
-            return new MaterialDialog
+            var schema = new StringSchema { Name = "Name", IconKind = PackIconKind.Account };
+            bool? result = await WindowFactory.FromSingleSchema("What is your name?", schema).Show();
+            if (result == true)
             {
-                Title = "Settings",
-                Form = new MaterialForm
-                {
-                    new StringSchema
-                    {
-                        Name = "Device name",
-                        Value = "Android"
-                    },
-                    new CaptionSchema
-                    {
-                        Name = "Connectivity"
-                    },
-                    new BooleanSchema
-                    {
-                        Name = "WiFi",
-                        IconKind = PackIconKind.Wifi,
-                        Value = true
-                    },
-                    new BooleanSchema
-                    {
-                        Name = "Mobile Data",
-                        IconKind = PackIconKind.Signal
-                    },
-                    new CaptionSchema
-                    {
-                        Name = "Device"
-                    },
-                    new NumberRangeSchema
-                    {
-                        Name = "Volume",
-                        IconKind = PackIconKind.VolumeHigh,
-                        MinValue = 0,
-                        MaxValue = 10,
-                        Value = 5
-                    },
-                    new KeyValueSchema
-                    {
-                        Name = "Ringtone",
-                        Value = "Over the horizon",
-                        IconKind = PackIconKind.MusicNote
-                    }
-                }
-            };
+                string name = schema.Value;
+                await WindowFactory.Alert($"Hello {name}!").Show();
+            }
         }
 
-        public static MaterialDialog EmailDialog()
+        private async void ShowModalInput(object sender, RoutedEventArgs e)
         {
-            return new MaterialDialog
+            var schema = new StringSchema { Name = "Name", IconKind = PackIconKind.Account };
+            bool? result = await DialogFactory.FromSingleSchema("What is your name?", schema).Show("RootDialog", LargeModalWidth);
+            if (result == true)
             {
-                Title = "Send e-mail",
-                PositiveAction = "SEND",
-                Form = new MaterialForm
-                {
-                    new StringSchema
-                    {
-                        Key = "to",
-                        Name = "To",
-                        IconKind = PackIconKind.Email
-                    },
-                    new StringSchema
-                    {
-                        Key = "message",
-                        Name = "Message",
-                        IsMultiLine = true,
-                        IconKind = PackIconKind.Comment
-                    }
-                },
-                OnPositiveAction = async session =>
-                {
-                    await TaskRunner.Run(async progress =>
-                    {
-                        progress.Message = "Sending e-mail...";
-                        await Task.Delay(1500);
-                        progress.Message = "Doing some work...";
-                        progress.Progress = 33d;
-                        await Task.Delay(1500);
-                        progress.Message = "Awaiting confirmation...";
-                        progress.Progress = 66d;
-                        await Task.Delay(1500);
-                        progress.Message = "E-mail sent...";
-                        progress.Progress = 100d;
-                        await Task.Delay(1500);
-                    }, session);
+                string name = schema.Value;
+                await DialogFactory.Alert($"Hello {name}!").Show("RootDialog", ModalWidth);
+            }
+        }
 
-                    session.Dialog.Message = "E-mail sent successfully.";
-                    session["to"] = "";
-                    session["message"] = "";
+        private async void ShowProgress(object sender, RoutedEventArgs e)
+        {
+            await TaskRunner.Run(async controller =>
+            {
+                for (var i = 1; i <= 100; i++)
+                {
+                    controller.Progress = i;
+                    await Task.Delay(50);
                 }
-            };
+            }, new ProgressDialogOptions
+            {
+                Message = "Processing data..."
+            });
+        }
+
+        private async void ShowModalProgress(object sender, RoutedEventArgs e)
+        {
+            await TaskRunner.Run(async controller =>
+            {
+                for (var i = 1; i <= 100; i++)
+                {
+                    controller.Progress = i;
+                    await Task.Delay(50);
+                }
+            }, new ProgressDialogOptions
+            {
+                Message = "Processing data..."
+            }, "RootDialog");
         }
     }
 }
---
+++ src/MaterialForms/Forms/DialogFactory.cs
@@ -0,0 +1,38 @@
+﻿namespace MaterialForms
+{
+    public static class DialogFactory
+    {
+        public static MaterialDialog Alert(string message) => Alert(message, null);
+
+        public static MaterialDialog Alert(string message, string title) => Alert(message, title, "OK");
+
+        public static MaterialDialog Alert(string message, string title, string action)
+        {
+            return new MaterialDialog(message, title, action, null);
+        }
+
+        public static MaterialDialog Prompt(string message) => Prompt(message, null);
+
+        public static MaterialDialog Prompt(string message, string title) => Prompt(message, title, "OK");
+
+        public static MaterialDialog Prompt(string message, string title, string positiveAction) => Prompt(message, title, positiveAction, "CANCEL");
+
+        public static MaterialDialog Prompt(string message, string title, string positiveAction, string negativeAction)
+        {
+            return new MaterialDialog(message, title, positiveAction, negativeAction);
+        }
+
+        public static MaterialDialog FromSingleSchema(SchemaBase schema) => FromSingleSchema(null, schema);
+
+        public static MaterialDialog FromSingleSchema(string message, SchemaBase schema) => FromSingleSchema(message, null, schema);
+
+        public static MaterialDialog FromSingleSchema(string message, string title, SchemaBase schema) => FromSingleSchema(message, title, "OK", schema);
+
+        public static MaterialDialog FromSingleSchema(string message, string title, string positiveAction, SchemaBase schema) => FromSingleSchema(message, title, positiveAction, "CANCEL", schema);
+
+        public static MaterialDialog FromSingleSchema(string message, string title, string positiveAction, string negativeAction, SchemaBase schema)
+        {
+            return new MaterialDialog(message, title, positiveAction, negativeAction, new MaterialForm(schema));
+        }
+    }
+}
