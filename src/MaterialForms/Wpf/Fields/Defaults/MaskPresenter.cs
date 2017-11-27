303848bcbd7ac84e34f23cc401f8b7f240e4d194
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-11-27 12:06:11 -0200

    Added mask presenter

---
+++ src/MaterialForms/Wpf/Fields/Defaults/MaskPresenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using MaterialForms.Wpf.Resources;
+
+namespace MaterialForms.Wpf.Fields.Defaults
+{
+    public class MaskPresenter : ValueBindingProvider
+    {
+        static MaskPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MaskPresenter),
+                new FrameworkPropertyMetadata(typeof(MaskPresenter)));
+        }
+
+        public MaskPresenter(IResourceContext context,
+            IDictionary<string, IValueProvider> fieldResources,
+            IDictionary<string, IValueProvider> formResources)
+            : base(context, fieldResources, formResources, true)
+        {
+        }
+    }
+}
