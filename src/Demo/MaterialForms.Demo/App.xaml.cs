23d4dacdeafac882443fe181be00589b690c1b7c
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-12-20 21:33:44 -0200

    Removed old stuff

--- src/Demo/MaterialForms.Demo/App.xaml.cs
+++ src/Demo/MaterialForms.Demo/App.xaml.cs
@@ -1,39 +1,36 @@
 ﻿using System;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
+using MaterialDesignThemes.Wpf;
 using MaterialForms.Demo.Infrastructure;
 using MaterialForms.Demo.Models;
 using MaterialForms.Mappers;
 using MaterialForms.Wpf.Annotations;
+using MaterialForms.Wpf.Controls;
 
-namespace MaterialForms.Demo
-{
+namespace MaterialForms.Demo {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
         [STAThread]
-        public static void Main()
-        {
-            var login = new Login();
-            var newLoginType = login.InjectAttributes(u => u.RememberMe, new[] {new FieldAttribute {Name = "Fuck"}});
-            var props = newLoginType.GetType().GetProperty("RememberMe")?.GetCustomAttributes();
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
+        public static void Main () {
+            var app = new App ();
+            app.InitializeComponent ();
+            app.Run ();
         }
 
         public DemoAppController Controller { get; }
 
-        public App()
-        {
-            Controller = new DemoAppController();
+        public App () {
+
+            Controller = new DemoAppController ();
         }
 
-        protected void OnStartup(object sender, StartupEventArgs e)
-        {
-            Controller.ShowApplicationWindow();
+        protected void OnStartup (object sender, StartupEventArgs e) {
+            Controller.ShowApplicationWindow ();
         }
     }
 }
