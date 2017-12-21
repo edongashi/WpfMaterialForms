574df0319eede4893e5ed2de302ed7d14fc91f1c
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-12-21 10:50:32 -0200

    Moved initialization to ModuleInit

---
+++ src/MaterialForms/Mappers/ModuleInitializer.cs
@@ -0,0 +1,16 @@
+﻿namespace MaterialForms.Mappers
+{
+    /// <summary>
+    /// Module initializer
+    /// </summary>
+    public static class ModuleInitializer
+    {
+        /// <summary>
+        /// Happens when this module is loaded
+        /// </summary>
+        public static void Initialize()
+        {
+            Mapper.InitializeIMapperClasses();
+        }
+    }
+}
