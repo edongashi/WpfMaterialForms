50a3ca66fa69128714a3a57e7e0a57e8cd4f7405
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-12-24 13:42:17 -0200

    Updated MaterialMapper

--- src/MaterialForms/Mappers/MaterialMapper.cs
+++ src/MaterialForms/Mappers/MaterialMapper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
 using Proxier.Mappers;
 
 namespace MaterialForms.Mappers
@@ -7,6 +9,42 @@ namespace MaterialForms.Mappers
     {
         protected MaterialMapper(Type type) : base(type)
         {
+        }
+
+        /// <summary>
+        ///     Add an attribute to a class.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        public void AddClassAttribute(params Expression<Func<Attribute>>[] expression)
+        {
+            var mapper = new Mapper(this)
+            {
+                PropertyInfo = null,
+                Expression = expression
+            };
+
+            Mappings.Add(mapper);
+        }
+
+        /// <summary>
+        ///     Adds a mapper by name.
+        /// </summary>
+        public void AddProperty(string prop, Type type)
+        {
+            Type = Type.InjectProperty(prop, type);
+        }
+
+        /// <summary>
+        ///     Adds a mapper by name.
+        /// </summary>
+        public void AddPropertyAttribute(string prop,
+            params Expression<Func<Attribute>>[] expression)
+        {
+            Mappings.Add(new Mapper(this)
+            {
+                Expression = expression,
+                PropertyInfo = Type.GetHighestProperty(prop)
+            });
         }
 
         public virtual void HandleAction(object model, string action, object parameter)
@@ -18,6 +56,40 @@ namespace MaterialForms.Mappers
     {
         public MaterialMapper() : base(typeof(TSource))
         {
+        }
+
+        /// <summary>
+        ///     Adds a mapper.
+        /// </summary>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="expression"></param>
+        /// <param name="propertyLambda"></param>
+        public void AddPropertyAttribute<TProperty>(Expression<Func<TSource, TProperty>> propertyLambda,
+            params Expression<Func<Attribute>>[] expression)
+        {
+            var type = Type;
+
+            if (!(propertyLambda.Body is MemberExpression member))
+                throw new ArgumentException(
+                    $"Expression '{propertyLambda}' refers to a method, not a property.");
+
+            var propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+                throw new ArgumentException(
+                    $"Expression '{propertyLambda}' refers to a field, not a property.");
+
+            if (type != propInfo.ReflectedType &&
+                !type.IsSubclassOf(propInfo.ReflectedType))
+                throw new ArgumentException(
+                    $"Expresion '{propertyLambda}' refers to a property that is not from type {type}.");
+
+            var mapper = new Mapper(this)
+            {
+                Expression = expression,
+                PropertyInfo = propInfo
+            };
+
+            Mappings.Add(mapper);
         }
 
         /// <summary>
