2edb45e2126e53f76b5dc1c93b47333edc5e2bcb
EdonGashi <edongashi@hotmail.co.uk>
2018-01-14 23:03:17 +0100

    ProgressField

---
+++ src/MaterialForms/Wpf/Annotations/Display/ProgressAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaterialForms.Wpf.Annotations.Display
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ProgressAttribute : Attribute
+    {
+        /// <summary>
+        /// Progress maximum value. Accepts a numeric value or a dynamic resource.
+        /// </summary>
+        public object Maximum { get; set; }
+    }
+}
